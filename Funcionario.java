3967c411c85a95edffadadcd54aebc3b9570e057
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2026-09-14 17:27:33 +0000

    111

--- Funcionario.java
+++ Funcionario.java
@@ -2,9 +2,20 @@ public class Funcionario{
     private String nome;
     private String cpf;
     private double salario;
+    private int tipo = 0;
+
+    public int getTipo(){
+        return tipo;
+    }
 
     public double getBonificacao(){
-        return this.salario * 0.1;
+        if (this.tipo == 0){
+            return this.salario * 0.1;
+        } else if (this.tipo == 1){
+            return this.salario * 1;
+        } else{
+            return this.salario * 2;
+        }
     }
 }
 
